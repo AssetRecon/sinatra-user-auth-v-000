cfa648c87ff23ffdc3334897c23249bf8b828713
Edsel Caprice <caprice@mail.usf.edu>
2018-11-10 15:34:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,11 +29,11 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
-    session[:id] = @user.id
-    if @user
-     session[:user_id] = @user.id
-     redirect '/users/home'
-   end
+      if @user
+        session[:user_id] = @user.id
+        redirect '/users/home'
+      end
+      redirect '/sessions/login'
   end
 
   get '/sessions/logout' do
