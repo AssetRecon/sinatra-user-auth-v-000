b03474428629ef3d4e61cebd52f1e9f13c49125f
Edsel Caprice <caprice@mail.usf.edu>
2018-11-10 15:32:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(params)
+    @user = User.find_by(email: params[:email], password: params[:password])
     session[:id] = @user.id
     if @user
      session[:user_id] = @user.id
